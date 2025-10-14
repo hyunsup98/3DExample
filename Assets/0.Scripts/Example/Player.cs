16f1eb0352f4d03a9bcdee443a55bb8595137f8d
Hyun <shs217shs@naver.com>
2025-10-14 12:35:46 +0900

    feat : 오브젝트 추가
    Player : 기체
    Bullet : 총알
    오브젝트 추가 제작

--- Assets/0.Scripts/Example/Player.cs
+++ Assets/0.Scripts/Example/Player.cs
@@ -4,59 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private bool isDone_Update;
-    private bool isDone_LateUpdate;
-    private bool isDone_FixedUpdate;
-
-    private void Awake()
-    {
-        Debug.Log($"Awake 호출");
-    }
-
     private void Start()
     {
-        Debug.Log($"Start 호출");
-    }
 
-    private void Update()
-    {
-        if (!isDone_Update)
-        {
-            isDone_Update = true;
-            Debug.Log($"Update 호출");
-        }
-    }
-
-    private void LateUpdate()
-    {
-        if (!isDone_LateUpdate)
-        {
-            isDone_LateUpdate = true;
-            Debug.Log($"LateUpdate 호출");
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (!isDone_FixedUpdate)
-        {
-            isDone_FixedUpdate = true;
-            Debug.Log($"FixedUpdate 호출");
-        }
-    }
-
-    private void OnEnable()
-    {
-        Debug.Log($"OnEnable 호출");
-    }
-
-    private void OnDisable()
-    {
-        Debug.Log($"OnDisable 호출");
-    }
-
-    private void OnDestroy()
-    {
-        Debug.Log($"OnDesstroy 호출");
     }
 }
